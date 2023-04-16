8eff40e571574a9c273c4a9556bbe99fdbf028ec
Kimber Ogden <mx.kimber@gmail.com>
2023-04-16 10:00:19 -0500

    add Post model, controller, and jbuilder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_145720) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_145919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_145720) do
 
   create_table "origins", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
