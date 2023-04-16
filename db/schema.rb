a035a5c0656ee7bea7aea4321bed3acc21af80c5
Kimber Ogden <mx.kimber@gmail.com>
2023-04-16 09:55:35 -0500

    add Ingredient model, controller, and jbuilder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_145236) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_145433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_145236) do
     t.integer "ingredient_id"
     t.integer "quantity"
     t.integer "crew_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "origin_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
