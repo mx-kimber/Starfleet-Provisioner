9f4ff7d39421e137bae2f5d297488bda5b9111f5
Kimber Ogden <mx.kimber@gmail.com>
2023-04-16 09:58:49 -0500

    add Category  model, controller, and jbuilder

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_145559) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_145720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "crew_members", force: :cascade do |t|
     t.string "name"
